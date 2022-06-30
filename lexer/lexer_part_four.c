98adcad83a89eb0d7d8ea9c460015c758bd24fd7
Abdellah Bellakrim <bellakrim2032@gmail.com>
2022-06-30 20:04:01 +0100

    handle in heredoc problem

--- lexer/lexer_part_four.c
+++ lexer/lexer_part_four.c
@@ -6,7 +6,7 @@
 /*   By: abellakr <abellakr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 19:59:21 by abellakr          #+#    #+#             */
-/*   Updated: 2022/06/28 22:09:47 by abellakr         ###   ########.fr       */
+/*   Updated: 2022/06/30 19:59:35 by abellakr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_data	*oranize_simple_command_list(t_data **simple_command_list)
 	backup = *simple_command_list;
 	new_list = NULL;
 	look_for_heredoc(backup, &new_list);
-	look_for_rip(backup, &new_list);
+	// look_for_rip(backup, &new_list); // look for it later
 	look_for_rop(backup, &new_list);
 	look_for_cmd(backup, &new_list);
 	new_list = join_cmds(&new_list);
@@ -34,7 +34,7 @@ void	look_for_heredoc(t_data *backup, t_data **new_list)
 {
 	while (backup)
 	{
-		if (backup->token == HEREDOC)
+		if (backup->token == HEREDOC || backup->token == RIP)
 			ft_lstadd_back_lexer(new_list, \
 			ft_lstnew_lexer(backup->str, backup->token));
 		backup = backup->next;
