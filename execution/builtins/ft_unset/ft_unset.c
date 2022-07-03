3373e2f1f05001acf9acd32d5e4c5b67f2b26ae1
marbenMB <mbenbajj@student.1337.ma>
2022-07-03 03:51:30 +0100

    Unset execution : unset env vars from env -> OK

--- execution/builtins/ft_unset/ft_unset.c
+++ execution/builtins/ft_unset/ft_unset.c
@@ -6,8 +6,64 @@
 /*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 13:44:53 by mbenbajj          #+#    #+#             */
-/*   Updated: 2022/06/28 13:45:41 by mbenbajj         ###   ########.fr       */
+/*   Updated: 2022/07/03 03:41:41 by mbenbajj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/execution.h"
+
+void    free_env_node(t_env *node)
+{
+    free(node->var);
+    free(node->value);
+    free(node);
+}
+
+void    unset_var(t_env **var)
+{
+    if ((*var)->prev)
+        (*var)->prev->next = (*var)->next;
+    if ((*var)->next)
+        (*var)->next->prev = (*var)->prev;
+    free_env_node(*var);
+}
+
+int valid_unset_name(char *name)
+{
+    int     idx;
+
+    idx = 0;
+    if (ft_isdigit(name[idx]))
+		return (1);
+	while (name[idx])
+	{
+		if (export_special_char(name[idx]))
+			return (1);
+		idx++;
+	}
+	return (0);
+}
+
+int ft_unset(t_shell *shell)
+{
+    int     idx;
+    t_env   *var;
+
+    idx = 1;
+    while (shell->cmd->cmd_flags[idx])
+    {
+        var = shell->env->next;
+        if (!valid_unset_name(shell->cmd->cmd_flags[idx]))
+        {    
+            while (var && ft_strcmp((var->var), shell->cmd->cmd_flags[idx]))
+                var = var->next;
+            if (var)
+                unset_var(&var);
+            ft_status(&shell->env, SUCC_STAT);
+        }
+        else
+            error_cmd_arg(&shell->env, "unset", shell->cmd->cmd_flags[idx], NVI);
+        idx++;
+    }
+    return (0);
+}
