51ffb86a4e3e004ab2381ebc0409e8cd491b01c2
Abdellah Bellakrim <bellakrim2032@gmail.com>
2022-07-03 19:31:30 +0100

    handle command argumants exapanding for export test like : export tt="1                      2    4                      3" | echo  -> sould print 1 2 4 3

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abellakr <abellakr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 02:41:00 by abellakr          #+#    #+#             */
-/*   Updated: 2022/07/03 16:37:42 by mbenbajj         ###   ########.fr       */
+/*   Updated: 2022/07/03 19:10:43 by abellakr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,9 +77,9 @@ int	main(void)
 		delete_here_doc_files(shell.heredoc_files);
 		free_tab((shell.heredoc_files));
 		// -------------------------------------------------------------- check leaks
-		// printf("\033[0;33m----------------------------\n");
-		// system("leaks minishell");
-		// printf("\n----------------------------\n\033[0m");
+		printf("\033[0;33m----------------------------\n");
+		system("leaks minishell");
+		printf("\n----------------------------\n\033[0m");
 	}
 	free_data2(&(shell.env));
 	return (0);
